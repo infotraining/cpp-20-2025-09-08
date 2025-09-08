1d63f275fe644551431d4bb9301b2d44eb4d9466
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-09-08 14:30:30 +0200

    <=> - Exercise - Done

--- _exercises/ex-compare/comparisons.cpp
+++ _exercises/ex-compare/comparisons.cpp
@@ -1,8 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
-#include <iostream>
-#include <vector>
-#include <string>
 #include <format>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std::literals;
 
@@ -26,59 +26,79 @@ struct Gadget
     std::string name;
     double price;
 
-    // TODO
+    std::strong_ordering operator<=>(const Gadget& other) const
+    {
+        if (auto cmp = name <=> other.name; cmp != 0)
+            return cmp;
+        return std::strong_order(price, other.price);
+    }
+
+    bool operator==(const Gadget& other) const
+    {
+        return name == other.name && (std::strong_order(price, other.price) == 0);
+    }
 };
 
 struct SuperGadget : Gadget
 {
     Rating rating;
 
-    // TODO
+    std::strong_ordering operator<=>(const SuperGadget& other) const = default;
 };
 
 TEST_CASE("Gadget - write custom operator <=> - stronger category than auto detected")
 {
-    // SECTION("==")
-    // {
-    //     CHECK(Gadget{"ipad", 1.0} == Gadget{"ipad", 1.0});
-    // }
-    
-    // SECTION("<=>")
-    // {
-    //     static_assert(std::is_same_v<decltype(Gadget{"ipad", 1.0} <=> Gadget{"ipad", 1.0}), std::strong_ordering>);
-        
-    //     CHECK(Gadget{"ipad", 1.0} <=> Gadget{"ipad", 1.0} == std::strong_ordering::equal);
-    // }
+    SECTION("==")
+    {
+        CHECK(Gadget{"ipad", 1.0} == Gadget{"ipad", 1.0});
+    }
+
+    SECTION("<=>")
+    {
+        static_assert(std::is_same_v<decltype(Gadget{"ipad", 1.0} <=> Gadget{"ipad", 1.0}), std::strong_ordering>);
+
+        CHECK(Gadget{"ipad", 1.0} <=> Gadget{"ipad", 1.0} == std::strong_ordering::equal);
+    }
 }
 
 TEST_CASE("SuperGadget - write custom operator <=> - member without compare-three-way operator")
 {
-    // CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} != SuperGadget{{"ipad", 1.0}, Rating{2}});
-    // CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} <=> SuperGadget{{"ipad", 1.0}, Rating{2}} == std::strong_ordering::less);    
+    CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} != SuperGadget{{"ipad", 1.0}, Rating{2}});
+    CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} <=> SuperGadget{{"ipad", 1.0}, Rating{2}} == std::strong_ordering::less);
 }
-
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-enum class RatingValue : uint8_t { very_poor = 1, poor, satisfactory, good, very_good, excellent};
+enum class RatingValue : uint8_t { very_poor = 1,
+    poor,
+    satisfactory,
+    good,
+    very_good,
+    excellent };
 
 struct RatingStar
 {
 public:
     RatingValue value;
 
-    explicit RatingStar(RatingValue rating_value) : value{rating_value}
-    {} 
+    explicit RatingStar(RatingValue rating_value)
+        : value{rating_value}
+    { }
 
-    // TODO
+    std::strong_ordering operator<=>(const RatingStar& other) const = default;
 };
+
+std::strong_ordering operator<=>(const RatingStar& ratingStar, const RatingValue& ratingValue)
+{
+    return ratingStar <=> RatingStar{ratingValue};
+}
 
 TEST_CASE("Rating Star - implement needed <=>")
 {
     RatingStar r1{RatingValue::good};
-    
-    // CHECK(r1 == RatingStar{RatingValue::good});
-    // CHECK(r1 <=> RatingStar{RatingValue::excellent} == std::strong_ordering::less);
-    // CHECK(r1 <=> RatingValue::excellent == std::strong_ordering::less);
-    // CHECK(RatingValue::excellent <=> r1 == std::strong_ordering::greater);
+
+    CHECK(r1 == RatingStar{RatingValue::good});
+    CHECK(r1 <=> RatingStar{RatingValue::excellent} == std::strong_ordering::less);
+    CHECK(r1 <=> RatingValue::excellent == std::strong_ordering::less);
+    CHECK(RatingValue::excellent <=> r1 == std::strong_ordering::greater);
 }
--- compare/comparisons.cpp
+++ compare/comparisons.cpp
@@ -16,20 +16,27 @@ struct Point
         return out << std::format("Point({},{})", p.x, p.y);
     }
 
-    bool operator==(const Point& other) const
-    {
-        return x == other.x && y == other.y;
-    }
+    bool operator==(const Point& other) const = default;
 
-    bool operator!=(const Point& other) const
-    {
-        return !(*this == other);
-    }
+    // bool operator!=(const Point& other) const
+    // {
+    //     return !(*this == other);
+    // }
 };
 
-struct Point3D
+bool operator==(const Point& p1, const std::pair<int, int>& p2)
 {
-    // TODO
+    return p1.x == p2.first && p1.y == p2.second;
+}
+
+struct Point3D : Point
+{
+    int z;
+
+    constexpr Point3D(int x, int y, int z) : Point{x, y}, z{z}
+    {}
+
+    bool operator==(const Point3D& other) const = default;
 };
 
 TEST_CASE("Point - operator ==")
@@ -41,25 +48,28 @@ TEST_CASE("Point - operator ==")
         Point p3{2, 1};
 
         CHECK(p1 == p2);
-        CHECK(p1 != p3);
+        CHECK(p1 != p3); // rewirite to !(p1 == p2)
+
+        CHECK(p1 == std::pair{1, 2});
+        CHECK(std::pair{1, 2} == p1);
+        CHECK(std::pair{1, 2} != p3);
     }
 
-    // SECTION("Point3D")
-    // {
-    //     Point3D p1{1, 2, 3};
-    //     Point3D p2{1, 2, 3};
-    //     Point3D p3{1, 2, 4};
+    SECTION("Point3D")
+    {
+        Point3D p1{1, 2, 3};
+        Point3D p2{1, 2, 3};
+        Point3D p3{1, 2, 4};
 
-    //     CHECK(p1 == p2);
-    //     CHECK(p1 != p3);
-    // }
+        CHECK(p1 == p2);
+        CHECK(p1 != p3);
+    }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 namespace Comparisons
 {
-
     struct Money
     {
         int dollars;
@@ -85,7 +95,7 @@ namespace Comparisons
             return out << std::format("${}.{}", m.dollars, m.cents);
         }
 
-        // TODO: add operator <=>
+        auto operator<=>(const Money& other) const = default;
     };
 
     namespace Literals
@@ -109,23 +119,49 @@ TEST_CASE("Money - operator <=>")
 
     SECTION("comparison operators are synthetized")
     {
-        // CHECK(m1 == m2);
-        // CHECK(m1 == Money(42.50));
-        // CHECK(m1 == 42.50_USD);
-        // CHECK(m1 != 42.51_USD);
-        // CHECK(m1 < 42.51_USD);
-        // CHECK(m1 <= 42.51_USD);
-        // CHECK(m1 > 0.99_USD);
-        // CHECK(m1 >= 0.99_USD);
+        CHECK(m1 == m2);
+        CHECK(m1 == Money(42.50));
+        CHECK(m1 == 42.50_USD);
+        CHECK(m1 != 42.51_USD);
+        CHECK(m1 < 42.51_USD);
+        CHECK(m1 <= 42.51_USD);
+        CHECK(m1 > 0.99_USD);
+        CHECK(m1 >= 0.99_USD);
 
-        // static_assert(Money{42, 50} == 42.50_USD);
+        static_assert(Money{42, 50} == 42.50_USD);
+
+        SECTION("it is possible to compare with integral type")
+        {
+            bool they_are_equal = m1 <=> m2 == 0;
+            CHECK(they_are_equal);
+
+            bool left_is_less = m1 <=> 42.51_USD < 0;
+            CHECK(left_is_less);
+        }
+
+        SECTION("we should prefer common syntax")
+        {
+            bool they_are_equal = m1 == m2;
+            CHECK(they_are_equal);
+
+            bool left_is_less = m1 < 42.51_USD; // (m1 <=> 42.51_USD < 0)
+            CHECK(left_is_less);
+        }
     }
 
     SECTION("sorting")
     {
-        // std::vector<Money> wallet{42.50_USD, 13.37_USD, 0.99_USD, 100.00_USD, 0.01_USD};
-        // std::ranges::sort(wallet);
-        // CHECK(std::ranges::is_sorted(wallet));
+        std::vector<Money> wallet{42.50_USD, 13.37_USD, 0.99_USD, 100.00_USD, 0.01_USD};
+        //std::ranges::sort(wallet);
+        std::sort(wallet.begin(), wallet.end(), std::less<>{});
+        CHECK(std::ranges::is_sorted(wallet));
+
+        std::vector<double> vec_that_floats{3.14, 5.77, 
+                                            std::numeric_limits<double>::quiet_NaN(), 88.66};
+        std::sort(vec_that_floats.begin(), vec_that_floats.end(), 
+                  [](auto a, auto b) { return std::strong_order(a, b) < 0; });
+
+        std::cout << std::format("sorted: {}", vec_that_floats) << "\n";
     }
 }
 
@@ -135,19 +171,23 @@ TEST_CASE("operator <=>")
     {
         int x = 42;
 
-        // TODO: add checks
+        auto result = x <=> 42;
+        static_assert(std::is_same_v<decltype(result), std::strong_ordering>);
+        CHECK(result == std::strong_ordering::equal);
     }
 
     SECTION("custom types")
     {
+        using Comparisons::Money;
+
         SECTION("result is a comparison category")
         {
-            // TODO
+            CHECK((Money{40, 10} <=> Money{40, 10} == 0));
         }
 
         SECTION("operators <, >, <=, >= are synthetized")
         {
-            // TODO
+            CHECK(Money{40, 10} < Money{40, 20});
         }
     }
 }
@@ -156,19 +196,86 @@ TEST_CASE("comparison categories")
 {
     SECTION("strong ordering")
     {
-        // TODO
+        auto result = 42 <=> 665;
+        static_assert(std::is_same_v<decltype(result), std::strong_ordering>);
     }
 
     SECTION("partial ordering")
     {
-        // TODO
+        double x = 3.14;
+        auto result = x <=> std::numeric_limits<double>::quiet_NaN();
+        //auto unordered_to_zero = std::partial_ordering::unordered == 0;
+        //CHECK(unordered_to_zero == false);
+        CHECK(x <=> std::numeric_limits<double>::quiet_NaN() 
+              == std::partial_ordering::unordered);;
+
+        static_assert(std::is_same_v<decltype(result), std::partial_ordering>);
+        CHECK((result == std::partial_ordering::unordered));
     }
 
     SECTION("weak ordering")
     {
-        // TODO
+        
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+struct Temperature
+{
+    double value;
+
+    auto operator<=>(const Temperature& other) const
+    {
+        return std::strong_order(value, other.value);
+    }
+
+    bool operator==(const Temperature& other) const
+    {
+        return std::strong_order(value, other.value) == 0;
+    }
+};
+
+struct PreCpp20
+{
+    int value;
+
+    // bool operator==(const PreCpp20& other) const
+    // {
+    //     return value == other.value;
+    // }
+
+    // bool operator<(const PreCpp20& other) const
+    // {
+    //     return value < other.value;
+    // }
+
+    auto operator<=>(const PreCpp20& other) const = default;
+};
+
+struct PostCpp20
+{
+    int id;
+    PreCpp20 member;
+
+    auto operator<=>(const PostCpp20& other) const = default;
+};
+
+TEST_CASE("custom types - <=>")
+{
+    Temperature t1{30.1};
+    Temperature t2{30.1};
+    Temperature t3{45.2};
+
+    CHECK(t1 == t2);
+    CHECK(t1 < t3);
+    CHECK(t3 >= t1);
+
+    PostCpp20 pc1{42, PreCpp20{665}};
+    PostCpp20 pc2{42, PreCpp20{42}};
+
+    CHECK(pc1 > pc2);
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -192,8 +299,20 @@ namespace Comparisons
             delete[] buffer_;
         }
 
-        // TODO: add comparisons operators
-    };
+        auto operator<=>(const Data& other) const
+        {
+            return std::lexicographical_compare_three_way(
+                buffer_, buffer_ + size_, other.buffer_, other.buffer_ + other.size_);
+        }
+
+        bool operator==(const Data& other) const
+        {
+            if (size_ != other.size_)
+                return false;
+
+            return std::equal(buffer_, buffer_ + size_, other.buffer_);
+        }
+     };
 } // namespace Comparisons
 
 TEST_CASE("lexicographical_compare_three_way")
@@ -203,4 +322,41 @@ TEST_CASE("lexicographical_compare_three_way")
     Data data1{1, 2, 3};
     Data data2{1, 2, 3};
     Data data3{1, 2, 4};
+
+    CHECK(data1 == data2);
+    CHECK(data1 < data3);
 }
+
+class Car
+{
+    std::string licence_plate_;
+    int milage_;
+public:
+    Car(const std::string& licence_plate, int milage) : licence_plate_{licence_plate}, milage_{milage}
+    {}
+
+    std::string get_licence_plate() const
+    {
+        return licence_plate_;
+    }
+
+    int get_milage()
+    {
+        return milage_;
+    }
+
+    void drive(int distance)
+    {
+        milage_ += distance;
+    }
+
+    std::weak_ordering operator<=>(const Car& other) const
+    {
+        return licence_plate_ <=> other.licence_plate_;
+    }
+
+    bool operator==(const Car& other) const
+    {
+        return licence_plate_ == other.licence_plate_;
+    }
+};
