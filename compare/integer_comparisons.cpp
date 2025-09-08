e8ff008d78316d8b3ebfe7f09e45921e9dbef6cb
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-09-08 10:14:37 +0200

    Integer comparisons - cmp_*

--- compare/integer_comparisons.cpp
+++ compare/integer_comparisons.cpp
@@ -4,10 +4,40 @@
 #include <string>
 #include <utility>
 
+using namespace std::literals;
+
+auto my_comparer(auto a, auto b) 
+{
+    if constexpr(std::integral<decltype(a)> 
+        && std::integral<decltype(b)>)
+        return std::cmp_less(a, b);
+    return a < b;
+}
+
 TEST_CASE("safe comparing integral numbers")
 {
     int x = -42;
     unsigned int y = 665;
 
-    //CHECK(x < y);
+    SECTION("direct comparison")
+    {
+        CHECK(std::cmp_less(x, y));
+        CHECK(std::in_range<int>(y));
+        CHECK(std::in_range<unsigned char>(x) == false);
+
+        //CHECK(std::cmp_greater("one"s, "two"s));
+    }
+
+    SECTION("with generic functions")
+    {
+        auto my_comparer = [](auto a, auto b) {
+            if constexpr(std::integral<decltype(a)> 
+                && std::integral<decltype(b)>)
+                return std::cmp_less(a, b);
+            return a < b;
+        };
+
+        CHECK(my_comparer(x, y));
+        CHECK(my_comparer("one"s, "two"s));
+    }
 }
