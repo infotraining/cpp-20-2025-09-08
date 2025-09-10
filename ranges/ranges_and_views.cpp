55747e95a2c28dba16f92a750fdc06e2ff864de3
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-09-10 15:20:27 +0200

    Enhance std::span and mdspan functionality with new utility functions and tests

--- ranges/ranges_and_views.cpp
+++ ranges/ranges_and_views.cpp
@@ -1,34 +1,53 @@
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
+#include <deque>
 #include <helpers.hpp>
 #include <iostream>
+#include <list>
+#include <map>
+#include <print>
 #include <ranges>
 #include <string>
 #include <vector>
 
 using namespace std::literals;
 
+void user_foo(std::ranges::range auto& container, auto projection)
+{
+    using TItem = std::ranges::range_value_t<decltype(container)>;
+    std::vector<TItem> vec;
+}
+
 TEST_CASE("ranges", "[ranges]")
 {
     auto data = helpers::create_numeric_dataset<20>(42);
     helpers::print(data, "data");
 
-    std::vector words = {"one"s, "two"s, "three"s, "four"s, "five"s, "six"s, "seven"s, "eight"s, "nine"s, "ten"s, 
-                         "eleven"s, "twelve"s, "thirteen"s, "fourteen"s, "fifteen"s, "sixteen"s, "seventeen"s, "eighteen"s, "nineteen"s, "twenty"s};
+    std::vector words = {"one"s, "two"s, "three"s, "four"s, "five"s, "six"s, "seven"s, "eight"s, "nine"s, "ten"s,
+        "eleven"s, "twelve"s, "thirteen"s, "fourteen"s, "fifteen"s, "sixteen"s, "seventeen"s, "eighteen"s, "nineteen"s, "twenty"s};
     helpers::print(words, "words");
 
     SECTION("algorithms")
     {
-        // TODO
+        std::ranges::sort(words, std::greater<>{});
+        CHECK(std::ranges::is_sorted(words, std::greater<>{}));
     }
 
     SECTION("projections")
     {
-        // TODO        
+        // std::sort(words.begin(), words.end(),
+        //           [](const auto& a, const auto& b) { return a.size() < b.size(); });
+
+        std::ranges::sort(words, std::less{}, &std::string::size);
+
+        helpers::print(words, "words sorted by size");
     }
 
     SECTION("concepts & tools")
     {
-        // TODO
+        int arr[256];
+
+        using T = std::ranges::range_value_t<decltype(arr)>;
     }
 }
 
@@ -46,43 +65,142 @@ TEST_CASE("sentinels", "[ranges]")
     std::vector data = {2, 3, 4, 1, 5, 42, 6, 7, 8, 9, 10};
 
     // TODO - sort range [begin; 42) in descending order
+    EndValue<42> my_sentinel;
+
+    std::ranges::sort(data.begin(), my_sentinel);
 
     helpers::print(data, "data");
+
+    std::contiguous_iterator auto pos = std::ranges::find(data.begin(), std::unreachable_sentinel, 42);
+
+    CHECK(*pos == 42);
 }
 
 TEST_CASE("views")
 {
-    std::vector data = {2, 3, 4, 1, 5, 42, 6, 7, 8, 9, 10};
+    std::deque data = {2, 3, 4, 1, 5, 42, 6, 7, 8, 9, 10};
 
     SECTION("all")
     {
+        auto all_view = std::views::all(data);
+        all_view[3] = 665;
+
+        auto another_view = all_view; // fast - O(1)
     }
 
     SECTION("subrange - iterator & sentinel as a view")
     {
+        auto head = std::ranges::subrange(data.begin(), EndValue<42>{});
+
+        std::ranges::sort(head);
+
+        for (auto& item : head)
+            item = 0;
+
+        helpers::print(head, "head");
+        helpers::print(data, "data");
     }
 
     SECTION("counted")
-    {        
+    {
+        auto tail = std::views::counted(data.rbegin(), 3);
+
+        std::ranges::fill(tail, 999);
+
+        helpers::print(data, "data");
     }
 
     SECTION("iota")
     {
+        auto gen = std::views::iota(1, 20);
+
+        for (const auto& item : gen)
+        {
+            std::cout << item << " ";
+        }
+        std::cout << "\n";
+    }
+
+    SECTION("single")
+    {
+        auto one_only = std::views::single(42);
+
+        for (const auto& item : one_only)
+            std::cout << item << "\n";
     }
 
     SECTION("pipes |")
     {
+        auto data = std::views::iota(1)
+            | std::views::take(20)
+            | std::views::filter([](int n) { return n % 2 == 0; })
+            | std::views::transform([](int n) { return n * n; })
+            | std::views::reverse;
+
+        helpers::print(data, "data");
     }
 }
 
 TEST_CASE("views - reference semantics")
-{    
-    std::vector data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+{
+    std::vector data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     auto evens_view = data | std::views::filter([](int i) { return i % 2 == 0; });
     helpers::print(data, "data");
 
-    // TODO - set all even numbers to 0 using evens_view
+    // for(auto& item : evens_view)
+    //     item = 0;
+
+    for (auto it = evens_view.begin(); it != evens_view.end(); ++it)
+    {
+        auto& item = *it;
+        item = 0;
+    }
+
+    // std::ranges::fill(evens_view, 0);
 
     helpers::print(data, "data");
+    std::print("data: {}\n", data);
+}
+
+TEST_CASE("maps & ranges")
+{
+    std::map<int, std::string> dict = {{1, "one"}, {2, "two"}};
+
+    helpers::print(dict | std::views::keys, "keys");
+    helpers::print(dict | std::views::values, "values");
+
+    auto keys_of_dict = dict | std::views::elements<0>;
+    helpers::print(keys_of_dict, "keys_of_dict");
+
+    for (const auto& key : dict | std::views::keys)
+    {
+        std::cout << key << "\n";
+    }
+}
+
+TEST_CASE("split")
+{
+    std::string text = "abc def ghi";
+
+    auto tokens_view = std::views::split(text, " ")
+        | std::views::transform([](auto token) { return std::string_view(token); })
+        | std::ranges::to<std::vector>();
+
+    helpers::print(tokens_view, "tokens");
+}
+
+TEST_CASE("enumerate")
+{
+    std::vector vec = {"zero", "one", "two", "three", "four"};
+
+    for (const auto& [index, value] : std::views::enumerate(vec))
+        std::cout << index << ": " << value << "\n";
+    
+    std::cout << "\n";
+
+    auto mapped = vec | std::views::enumerate | std::ranges::to<std::map>();
+
+    for (const auto& [index, value] : mapped)
+        std::cout << index << ": " << value << "\n";
 }
--- stdlib-features/std_lib_cpp20.cpp
+++ stdlib-features/std_lib_cpp20.cpp
@@ -1,11 +1,146 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-#include <vector>
+#include <span>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <numbers>
+#include <mdspan>
+#include <print>
 
 using namespace std::literals;
 
+void zero(std::span<int> sp, int default_value = 0)
+{
+    for(auto& item : sp)
+        item = default_value;
+}
+
+void print(std::span<const int> sp)
+{
+    std::cout << "[ ";
+    for(auto& item : sp)
+        std::cout << item << " ";
+    std::cout << "]";
+}
+
 TEST_CASE("std::span")
 {
-    REQUIRE(true);
+    int buffer[256];
+
+    SECTION("compile time extent")
+    {
+        std::span buffer_span_1{buffer};
+        CHECK(buffer_span_1.size() == 256);
+
+        zero(buffer_span_1);
+    }
+
+    SECTION("dynamic extent")
+    {
+        std::span<int> buffer_span_2{buffer};
+        CHECK(buffer_span_2.size() == 256);
+
+        zero(buffer_span_2);
+        CHECK(std::ranges::all_of(buffer, [](int x) { return x == 0;}));
+
+        zero(buffer);
+        CHECK(std::ranges::all_of(buffer, [](int x) { return x == 0;}));
+
+        std::vector<int> vec = {1, 2, 3, 4};
+        zero(std::span{vec.data() + 1, 2});
+        CHECK(vec == std::vector{1, 0, 0, 4});
+
+        print(vec);
+    }
+}
+
+TEST_CASE("BEWARE! Dangling pointers!!!")
+{
+    std::vector<int> vec = {1, 2, 3, 4};
+    std::span sp_vec{vec};
+
+    print(sp_vec);
+
+    vec.push_back(5);
+
+    print(sp_vec);
+}
+
+void print_as_bytes(const float f, const std::span<const std::byte> bytes)
+{
+#ifdef __cpp_lib_format
+    std::cout << std::format("{:+6}", f) << " - { ";
+
+    for (std::byte b : bytes)
+    {
+        std::cout << std::format("{:02X} ", std::to_integer<int>(b));
+    }
+
+    std::cout << "}\n";
+#endif
+}
+
+namespace Explain
+{
+    template <typename T>
+    constexpr static T Pi_v(3.141592653589793);
+}
+
+TEST_CASE("float as span of bytes")
+{
+    float data[] = {std::numbers::pi_v<float>};
+    //float data[] = {Explain::Pi_v<float>};
+
+    std::span<const std::byte> const_bytes = std::as_bytes(std::span{data});
+    print_as_bytes(data[0], const_bytes);
+
+    std::span<std::byte> writeble_bytes = std::as_writable_bytes(std::span{data});
+    writeble_bytes[3] |= std::byte{0b1000'0000};
+    print_as_bytes(data[0], const_bytes);
+}
+
+TEST_CASE("mdspan")
+{
+    std::vector v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+    // View data as contiguous memory representing 2 rows of 6 ints each
+    auto ms2 = std::mdspan(v.data(), 2, 6);
+    
+    // View the same data as a 3D array 2 x 3 x 2
+    auto ms3 = std::mdspan(v.data(), 2, 3, 2);
+
+    // Write data using 2D view
+    for (std::size_t i = 0; i != ms2.extent(0); i++)
+        for (std::size_t j = 0; j != ms2.extent(1); j++)
+            ms2[i, j] = i * 1000 + j;
+
+    // Read back using 3D view
+    for (std::size_t i = 0; i != ms3.extent(0); i++)
+    {
+        std::println("slice @ i = {}", i);
+        for (std::size_t j = 0; j != ms3.extent(1); j++)
+        {
+            for (std::size_t k = 0; k != ms3.extent(2); k++)
+                std::print("{} ", ms3[i, j, k]);
+            std::println("");
+        }
+    }
+}
+
+TEST_CASE("vector never shrins implcitly")
+{
+    std::vector<int> vec;
+    CHECK(vec.size() == 0);
+    CHECK(vec.capacity() == 0);
+
+    for(int i = 0; i < 1'000'000; ++i)
+        vec.push_back(i);
+    CHECK(vec.size() == 1'000'000);
+    CHECK(vec.capacity() >= 1'000'000);
+
+    vec.clear();
+    CHECK(vec.size() == 0);
+    vec.shrink_to_fit();
+    CHECK(vec.capacity() == 0);
 }
